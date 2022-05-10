30532c63f4f4bd2bc1db8e71a78176432a30f5d6
bobbyrosehag <bobbyrosehag@gmail.com>
2022-05-10 15:55:06 +0200

    Initial commit

---
+++ backend/utils/testQuestion.mjs
@@ -0,0 +1,9 @@
+//Used to add as a test question
+const testQuestion = {
+  id: 0,
+  category: "HTML",
+  question: "What does HTML stand for",
+  answer: "Hypertext Markup Language",
+};
+
+export default testQuestion;
