847f3c2bc492150b5477916357c9b8ded90e738a
bobbyrosehag <bobbyrosehag@gmail.com>
2022-05-31 15:44:58 +0200

    Hotfix

--- backend/server.js
+++ backend/server.js
@@ -16,15 +16,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/questions", require("./routes/questionRoutes.js"));
 app.use("/api/users", require("./routes/userRoutes.js"));
 
-//Serve Frontend
+// Serve frontend
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    );
-  });
+    )
+  );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
 }
